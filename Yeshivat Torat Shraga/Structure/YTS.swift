2b80c3669425914bf223e8c69267b357922cf865
David Reese <david@reesedevelopment.com>
2021-11-10 18:45:35 +0200

    documentation and some updates

--- Yeshivat Torat Shraga/Structure/YTS.swift
+++ Yeshivat Torat Shraga/Structure/YTS.swift
@@ -12,17 +12,34 @@ typealias FirestoreID = String
 typealias FileID = String
 
 class Rabbi {
-    var firestoreID: FirestoreID
+    /// The `FirestoreID` associated with this object in Firestore
+    private var firestoreID: FirestoreID
+    
+    /// The name associated with this object
     var name: String
+    
+    /// The profile image associated with this object
     var profileImage: Image?
+    
+    /// The `URL` that links to  this object's profile image
     var profileImageURL: URL?
     
+    /// Standard initalizer used for a preexisting `Image`
+    /// - Parameters:
+    ///   - firestoreID: The `FirestoreID` associated with this object in Firestore
+    ///   - name: The name associated with this object
+    ///   - profileImage: The profile image associated with this object
     init(id firestoreID: FirestoreID, name: String, profileImage: Image) {
         self.firestoreID = firestoreID
         self.name = name
         self.profileImage = profileImage
     }
     
+    /// Standard initalizer used when `URL` for a profile image is available and an `Image` is not
+    /// - Parameters:
+    ///   - firestoreID: The `FirestoreID` associated with this object in Firestore
+    ///   - name: The name associated with this object
+    ///   - profileImageURL: The `URL` that links to this object's profile image
     init(id firestoreID: FirestoreID, name: String, profileImageURL: URL) {
         self.firestoreID = firestoreID
         self.name = name
@@ -48,15 +65,34 @@ class Rabbi {
 
 typealias Tag = String
 
+/// A content object modeled like a Firestore content document
 protocol YTSContent {
+    /// The `FirestoreID` associated with this object in Firestore
     var firestoreID: FirestoreID { get }
+    
+    /// The `FileID` associated with this object's files in Firestore
     var fileID: FileID { get }
+    
+    /// The `URL` that links to the source content
     var sourceURL: URL { get }
+    
+    /// The content title
     var title: String { get }
+    
+    /// The `Rabbi` object credited with authoring this object
     var author: Rabbi { get }
+    
+    /// The content description
     var description: String { get }
+    
+    /// The time that this content was uploaded to the server
+    /// - Note: This date can be manually manipulated on the server end, so it may not be the actual upload date
     var date: Date { get }
+    
+    /// The duration of the content in seconds
     var duration: TimeInterval { get }
+    
+    /// `Tag` references to this object's topics
     var tags: [Tag] { get }
 }
 
@@ -74,6 +110,18 @@ class Video: YTSContent {
     var thumbnail: Image?
     var thumbnailURL: URL?
     
+    /// Standard initializer for a `Video`
+    /// - Parameters:
+    ///   - firestoreID: The `FirestoreID` associated with this object in Firestore
+    ///   - fileID: The `FileID` associated with this object's files in Firestore
+    ///   - sourceURL: The `URL` that links to the source content
+    ///   - title: The content title
+    ///   - author: The `Rabbi` object credited with authoring this object
+    ///   - description: The content description
+    ///   - date: The time that this content was uploaded to the server
+    ///   - duration: The duration of the content in seconds
+    ///   - tags: `Tag` references to this object's topics
+    ///   - thumbnail: The thumbnail associated with this content
     init(id firestoreID: FirestoreID, fileID: FileID, sourceURL: URL, title: String, author: Rabbi, description: String, date: Date, duration: TimeInterval, tags: [Tag], thumbnail: Image) {
         self.firestoreID = firestoreID
         self.fileID = fileID
@@ -88,6 +136,18 @@ class Video: YTSContent {
         self.thumbnail = thumbnail
     }
     
+    /// Standard initializer for a `Video`
+    /// - Parameters:
+    ///   - firestoreID: The `FirestoreID` associated with this object in Firestore
+    ///   - fileID: The `FileID` associated with this object's files in Firestore
+    ///   - sourceURL: The `URL` that links to the source content
+    ///   - title: The content title
+    ///   - author: The `Rabbi` object credited with authoring this object
+    ///   - description: The content description
+    ///   - date: The time that this content was uploaded to the server
+    ///   - duration: The duration of the content in seconds
+    ///   - tags: `Tag` references to this object's topics
+    ///   - thumbnailURL: The `URL` associated with this content's thumbnail image
     init(id firestoreID: FirestoreID, fileID: FileID, sourceURL: URL, title: String, author: Rabbi, description: String, date: Date, duration: TimeInterval, tags: [Tag], thumbnailURL: URL) {
         self.firestoreID = firestoreID
         self.fileID = fileID
@@ -114,6 +174,17 @@ class Audio: YTSContent {
     var duration: TimeInterval
     var tags: [Tag]
     
+    /// Standard initializer for an `Audio`
+    /// - Parameters:
+    ///   - firestoreID: The `FirestoreID` associated with this object in Firestore
+    ///   - fileID: The `FileID` associated with this object's files in Firestore
+    ///   - sourceURL: The `URL` that links to the source content
+    ///   - title: The content title
+    ///   - author: The `Rabbi` object credited with authoring this object
+    ///   - description: The content description
+    ///   - date: The time that this content was uploaded to the server
+    ///   - duration: The duration of the content in seconds
+    ///   - tags: `Tag` references to this object's topics
     init(id firestoreID: FirestoreID, fileID: FileID, sourceURL: URL, title: String, author: Rabbi, description: String, date: Date, duration: TimeInterval, tags: [Tag]) {
         self.firestoreID = firestoreID
         self.fileID = fileID
@@ -127,7 +198,10 @@ class Audio: YTSContent {
     }
 }
 
+/// An enhanced wrapper on a `Tag` which includes an icon
 struct Category {
     var tag: Tag
+    
+    /// The `Image` associated with this `Tag`
     var icon: Image
 }
