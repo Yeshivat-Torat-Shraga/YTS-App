acad871ecfb649b07c361d220b373df8f64201a2
David Reese <david@reesedevelopment.com>
2021-11-15 22:29:17 +0200

    progress on loadRabbis function

--- Yeshivat Torat Shraga/Code/Structure/FirebaseConnection.swift
+++ Yeshivat Torat Shraga/Code/Structure/FirebaseConnection.swift
@@ -6,9 +6,23 @@
 //
 
 import Foundation
+import Firebase
 
 final class FirebaseConnection {
+    static var functions = Functions.functions()
+    
+    /// Loads `Rabbi` objects from Firestore
+    /// - Parameters:
+    ///   - lastLoadedDocumentID: Pages results starting from first element afterwards
+    ///   - requestedCount: The amount of `Rabbi` objects to return. Default is `10`
+    ///   - completion: Callback which returns the results and metadata once function completes, including the new `lastLoadedDocumentID`
     static func loadRabbis(lastLoadedDocumentID: FirestoreID? = nil, requestedCount: Int = 10, completion: (_ results: (rabbis: [Rabbi], metadata: (newLastLoadedDocumentID: FirestoreID, includesLastElement: Bool))?, _ error: Error?) -> Void) {
         
+        let data = NSDictionary(dictionary: ["count": requestedCount])
+        
+        let httpsCallable = functions.httpsCallable("loadRabbis")
+        
+        httpsCallable.call(data) { callResult, callError in
+        }
     }
 }
--- Yeshivat Torat Shraga/Code/functions/index.js
+++ Yeshivat Torat Shraga/Code/functions/index.js
@@ -1,9 +1,84 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+	projectId: "yeshivat-torat-shraga"
+});
+
+exports.loadRabbis = functions.https.onCall(async (callData, context) => {
+	// if (context.app == undefined) {
+	// 	throw new functions.https.HttpsError(
+	// 		'failed-precondition',
+	// 		'The function must be called from an App Check verified app.')
+	// }
+
+	let pastDocumentID = callData.lastLoadedDocumentID;
+	let requestedCount = callData.count || 10;
+
+	let rabbis = [];
+
+	let db = admin.firestore();
+	let query = db.collection('rabbis').orderBy('name', 'asc');
+
+	if (typeof pastDocumentID == "string") {
+		let snapshot = await db.collection("rabbis").doc(pastDocumentID).get()
+		query = query.startAfter(snapshot);
+		log(`Starting after document '${snapshot}'`);
+	}
+
+	let rabbisSnapshot = await query.limit(requestedCount).get();
+
+	await Promise.all(rabbisSnapshot.docs.map(async doc => {
+		const data = doc.data();
+		const pfpID = data.profile_picture_id;
+
+		const bucket = admin.storage().bucket('yeshivat-torat-shraga.appspot.com');
+    log(JSON.stringify(bucket));
+	  const url = await bucket.file('adavid_lp-2.jpg').getSignedUrl({
+			action: "read",
+			expires: "11-20-2021",
+		});
+
+		const r = {
+			id: doc.id,
+			name: data.name,
+			profile_picture_url: url
+		};
+
+    rabbis.push(r);
+	}));
+
+  return {
+    rabbis: rabbis
+  };
+});
+
+exports.loadContent = functions.https.onCall(async (callData, context) => {
+  // if (context.app == undefined) {
+	// 	throw new functions.https.HttpsError(
+	// 		'failed-precondition',
+	// 		'The function must be called from an App Check verified app.')
+	// }
+
+	let pastDocumentID = callData.lastLoadedDocumentID;
+	let requestedCount = callData.count || 10;
+
+  let content = [];
+
+	let db = admin.firestore();
+	let query = db.collection('content').orderBy('date', 'asc');
+
+	if (typeof pastDocumentID == "string") {
+		let snapshot = await db.collection("content").doc(pastDocumentID).get()
+		query = query.startAfter(snapshot);
+		log(`Starting after document '${snapshot}'`);
+	}
+
+	let rabbisSnapshot = await query.limit(requestedCount).get();
+});
+
+function log(data, structured = true) {
+	functions.logger.info(data, {
+		structuredData: structured
+	});
+}
