28ff82675c498605c97a2b99b61fc610e064b2f7
David Reese <david@reesedevelopment.com>
2021-11-22 15:04:36 +0200

    added audio player struct

---
+++ Yeshivat Torat Shraga/Code/Views/AudioPlayer.swift
@@ -0,0 +1,22 @@
+//
+//  AudioPlayer.swift
+//  Yeshivat Torat Shraga
+//
+//  Created by David Reese on 11/22/21.
+//
+
+import SwiftUI
+
+struct AudioPlayer: View {
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AudioPlayer_Previews: PreviewProvider {
+    static var previews: some View {
+        AudioPlayer()
+    }
+}
