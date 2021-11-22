ad186ada02d998cfbff8224794d5247c72919e66
David Reese <david@reesedevelopment.com>
2021-11-22 21:28:52 +0200

    work on audioplayer

--- Yeshivat Torat Shraga/Code/Views/AudioPlayer.swift
+++ Yeshivat Torat Shraga/Code/Views/AudioPlayer.swift
@@ -28,7 +28,12 @@ struct AudioPlayer: View {
         }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            self.player.play()
+        } label: {
+            Image(systemName: "play.fill")
+        }
+
     }
 }
 
