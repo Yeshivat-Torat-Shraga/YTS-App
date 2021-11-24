15ea460e4265b6350ec08587598ebe424ef34abd
Benji Tusk <benjitusk1@gmail.com>
2021-11-24 21:46:57 +0200

    Added extension to Date

--- Yeshivat Torat Shraga/Code/Tools/Helpers.swift
+++ Yeshivat Torat Shraga/Code/Tools/Helpers.swift
@@ -36,3 +36,44 @@ func timeFormattedMini(totalSeconds: TimeInterval) -> String {
         return str
     }
 }
+
+extension Date {
+    func get(_ components: Calendar.Component..., calendar: Calendar = Calendar.current) -> DateComponents {
+        return calendar.dateComponents(Set(components), from: self)
+    }
+    
+    func get(_ component: Calendar.Component, calendar: Calendar = Calendar.current) -> Int {
+        return calendar.component(component, from: self)
+    }
+    
+    static func monthNameFor(_ number: Int) -> String? {
+        switch number {
+        case 1:
+            return "January"
+        case 2:
+            return "Feburary"
+        case 3:
+            return "March"
+        case 4:
+            return "April"
+        case 5:
+            return "May"
+        case 6:
+            return "June"
+        case 7:
+            return "July"
+        case 8:
+            return "August"
+        case 9:
+            return "September"
+        case 10:
+            return "October"
+        case 11:
+            return "November"
+        case 12:
+            return "December"
+        default:
+            return nil
+        }
+    }
+}
