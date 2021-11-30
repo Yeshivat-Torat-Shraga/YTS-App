56c539d455c05ef0cc89a5148b330849515189f4
David Reese <david@reesedevelopment.com>
2021-11-30 13:33:27 +0200

    allowed arbitrary loads and small UI change

--- Yeshivat Torat Shraga/Code/Views/PlayBar.swift
+++ Yeshivat Torat Shraga/Code/Views/PlayBar.swift
@@ -26,8 +26,8 @@ struct PlayBar: View {
                     .background(Color(UIColor.systemGray5))
                     .cornerRadius(3)
                     .padding(.vertical, 10)
-                    .padding(.horizontal)
-                    .shadow(radius: 2)
+                    .padding(.leading)
+                    .shadow(radius: 1)
                 VStack {
                     HStack {
                         Text(audioCurrentlyPlaying.title)
@@ -43,7 +43,7 @@ struct PlayBar: View {
                 HStack {
                     if RootModel.audioPlayer.player.timeControlStatus == .playing {
                         Button(action: {
-                            RootModel.audioPlayer.player.pause()
+                            RootModel.audioPlayer.pause()
                             self.model.objectWillChange.send()
                         }, label: {
                             Image(systemName: "pause.fill")
@@ -52,7 +52,7 @@ struct PlayBar: View {
                         })
                     } else if RootModel.audioPlayer.player.timeControlStatus == .paused {
                         Button(action: {
-                            RootModel.audioPlayer.player.play()
+                            RootModel.audioPlayer.play()
                             self.model.objectWillChange.send()
                         }, label: {
                             Image(systemName: "play.fill")
