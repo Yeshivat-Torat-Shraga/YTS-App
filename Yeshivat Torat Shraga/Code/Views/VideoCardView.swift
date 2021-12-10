02fe59e45cea36a5125e4cd7829b593c90099d35
Benji Tusk <benjitusk1@gmail.com>
2021-12-10 02:43:57 +0200

    Edited up Audio and Video cards for visual symmetry

--- Yeshivat Torat Shraga/Code/Views/VideoCardView.swift
+++ Yeshivat Torat Shraga/Code/Views/VideoCardView.swift
@@ -16,89 +16,63 @@ struct VideoCardView: View {
     }
     
     var body: some View {
-        HStack {
-            DownloadableImage(object: model.video)
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 75, height: 100)
-            //                .clipped()
-                .cornerRadius(10, corners: [.topLeft, .bottomLeft])
-            
-            // Rest of card goes here:
-            VStack {
-                HStack {
-                    //                    Circle()
-                    //                        .fill(LinearGradient(
-                    //                            gradient: Gradient(
-                    //                                stops: [
-                    //                                    Gradient.Stop(
-                    //                                        color: Color(
-                    //                                            hue: 0.610,
-                    //                                            saturation: 0.5,
-                    //                                            brightness: 0.19),
-                    //                                        location: 0),
-                    //                                    Gradient.Stop(
-                    //                                        color: Color(
-                    //                                            hue: 0.616,
-                    //                                            saturation: 0.431,
-                    //                                            brightness: 0.510),
-                    //                                        location: 1)]),
-                    //                            startPoint: UnitPoint.bottomLeading,
-                    //                            endPoint: UnitPoint.trailing))
-                    //                        .frame(width: 35, height: 35)
-                    //                        .overlay(
-                    //                            Image(systemName: "play")
-                    //                                .foregroundColor(Color("ShragaGold"))
-                    //                        )
-                    VStack(alignment: .leading) {
-                        Text(model.video.name)
-                            .font(.title3)
-                            .bold()
-                            .foregroundColor(.primary)
+        Button {
+//            RootModel.videoPlayer.set(video: model.video)
+            isShowingPlayerSheet = true
+        } label: {
+            HStack {
+                DownloadableImage(object: model.video)
+                    .overlay(Color.black.opacity(0.2))
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 125, height: 105)
+                    .cornerRadius(10, corners: [.topLeft, .bottomLeft])
+                
+                // Rest of card goes here:
+                VStack {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(model.video.name)
+                                .font(.title3)
+                                .bold()
+                                .foregroundColor(.primary)
+                        }
+                        
+                        Spacer()
                     }
                     
                     Spacer()
                     
-                    Button {
-                        //                        RootModel.videoPlayer.set(video: model.video)
-                        //                        isShowingPlayerSheet = true
-                    } label: {
-                        Image(systemName: "play.circle.fill")
-                            .resizable()
-                            .shadow(radius: 1)
-                    }
-                    .frame(width: 35, height: 35)
-                    .padding()
-                }
-                HStack {
                     HStack {
-                        if let date = model.video.date {
-                            if let month = Date.monthNameFor(date.get(.month)) {
-                                HStack {
-                                    let yearAsString = String(date.get(.year))
-                                    Image(systemName: "calendar")
-                                    Text("\(month) \(date.get(.day)), \(yearAsString)")
-                                        .foregroundColor(Color("Gray"))
+                        HStack {
+                            if let date = model.video.date {
+                                if let month = Date.monthNameFor(date.get(.month)) {
+                                    HStack {
+                                        let yearAsString = String(date.get(.year))
+                                        Image(systemName: "calendar")
+                                        Text("\(month) \(date.get(.day)), \(yearAsString)")
+                                            .foregroundColor(Color("Gray"))
+                                    }
                                 }
                             }
                         }
+                        Spacer()
+                        HStack {
+                            Text(timeFormattedMini(totalSeconds: model.video.duration ?? 0))
+                                .foregroundColor(Color("Gray"))
+                            Image(systemName: "clock")
+                        }
+                        .padding(.trailing, 5)
                     }
-                    Spacer()
-                    HStack {
-                        Text(timeFormattedMini(totalSeconds: model.video.duration ?? 0))
-                            .foregroundColor(Color("Gray"))
-                        Image(systemName: "clock")
-                    }
-                    .padding(.trailing, 5)
+                    .font(.footnote)
                 }
-                .font(.footnote)
+                
+                .padding()
             }
-            
-            .padding(.trailing)
         }
+        .frame(height: 105)
         .background(
             Rectangle()
                 .fill(Color(UIColor.systemBackground))
-                .opacity(01)
                 .cornerRadius(10)
                 .shadow(radius: 2)
         )
@@ -107,11 +81,16 @@ struct VideoCardView: View {
 
 struct VideoCardView_Previews: PreviewProvider {
     static var previews: some View {
-        VStack {
-            AudioCardView(audio: .sample)
-            VideoCardView(video: .sample)
-        }
-        .padding()
+        ScrollView {
+            VStack {
+                AudioCardView(audio: .sample)
+                VideoCardView(video: .sample)
+                AudioCardView(audio: .sample)
+                VideoCardView(video: .sample)
+            }
+            .previewLayout(.sizeThatFits)
+            .padding()
         .foregroundColor(Color("ShragaBlue"))
+        }
     }
 }
