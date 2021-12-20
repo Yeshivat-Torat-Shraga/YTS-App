621c0d19d6868b1e50f9e0e7a69563dd11360632
Benji Tusk <benjitusk1@gmail.com>
2021-12-20 12:25:31 +0200

    Made TagView

--- Yeshivat Torat Shraga/Code/Views/TagView.swift
+++ Yeshivat Torat Shraga/Code/Views/TagView.swift
@@ -17,15 +17,55 @@ struct TagView: View {
     }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            VStack {
+                HStack() {
+                    Text(tag.name)
+                        .fontWeight(.bold)
+                        .font(.largeTitle)
+                        .padding()
+                    Spacer()
+                }
+                Spacer()
+                ScrollView {
+                    VStack {
+                        HStack {
+                            Text("Recently Uploaded")
+                                .font(.title3)
+                                .bold()
+                            Spacer()
+                        }
+                        if let sortables = model.sortables {
+                            ForEach(sortables, id: \.self) { sortable in
+                                if let video = sortable.video {
+                                    VideoCardView(video: video)
+                                        .contextMenu {
+                                            Button("Play") {}
+                                        }
+                                } else if let audio = sortable.audio {
+                                    AudioCardView(audio: audio)
+                                        .contextMenu {
+                                            Button("Play") {}
+                                        }
+                                }
+                            }
+                        }
+                    }
+                    .padding([.horizontal, .top])
+                }
+                Spacer()
+            }
+            .background(
+                Blur(style: .systemThinMaterial).edgesIgnoringSafeArea(.vertical)
+            )
+            .onAppear {
+                self.model.load()
+            }
     }
 }
 
 struct TagView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            TagView(tag: .sample)
-                .navigationTitle(Tag.sample.name)
-        }
+        TagView(tag: .sample)
+            .background(LinearGradient(gradient: Gradient(stops: [Gradient.Stop(color: Color(hue: 0.1625910379800452, saturation: 0.8462154434387943, brightness: 0.8311787341014449, opacity: 1.0), location: 0.5724087641789364), Gradient.Stop(color: Color(hue: 0.6322852444935995, saturation: 0.8108159720179547, brightness: 0.7733789007347751, opacity: 1.0), location: 0.572469740647536)]), startPoint: UnitPoint.leading, endPoint: UnitPoint.top))
     }
 }
