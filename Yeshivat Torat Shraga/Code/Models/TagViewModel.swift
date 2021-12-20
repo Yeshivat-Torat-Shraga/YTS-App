e67277e675b19ac735c634d27be568ad634735cc
Benji Tusk <benjitusk1@gmail.com>
2021-12-20 12:19:58 +0200

    TagViewModel loads proper content from firebase

--- Yeshivat Torat Shraga/Code/Models/TagViewModel.swift
+++ Yeshivat Torat Shraga/Code/Models/TagViewModel.swift
@@ -6,11 +6,57 @@
 //
 
 import Foundation
+import SwiftUI
 
 class TagViewModel: ObservableObject {
     let tag: Tag
-    
+    @Published var sortables: [SortableYTSContent]?
+    @Published var content: Content?
     init(tag: Tag) {
         self.tag = tag
+    }
+    
+    func load() {
+        FirebaseConnection.loadContent(
+            searchData: ["field": "tag",
+                         "value": self.tag.name.lowercased()],
+            includeThumbnailURLs: false
+        ) { results, error in
+                guard let results = results else {
+//                    self.showError(error: error ?? YTSError.unknownError, retry: self.load)
+                    fatalError(error!.localizedDescription)
+                }
+                print(results)
+                withAnimation {
+                    self.content = results.content
+                    
+                    var sortables: [SortableYTSContent] = []
+                    for audio in self.content!.audios {
+                        sortables.append(audio.sortable)
+                    }
+                    for video in self.content!.videos {
+                        sortables.append(video.sortable)
+                    }
+                    
+                    self.sortables = sortables.sorted(by: { lhs, rhs in
+                        return lhs.date! > rhs.date!
+                    })
+                    
+                    
+                }
+                
+//                DispatchQueue.global(qos: .background).async {
+//                    for audio in self.content!.audios {
+//                        if !(audio.author is DetailedRabbi) {
+//                            audio.author = self.rabbi
+//                        }
+//                    }
+//                    for video in self.content!.videos {
+//                        if !(video.author is DetailedRabbi) {
+//                            video.author = self.rabbi
+//                        }
+//                    }
+//                }
+            }
     }
 }
