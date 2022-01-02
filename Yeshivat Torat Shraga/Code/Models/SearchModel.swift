f00e2de88a286627b8364dd04519cd60393f54bf
Benji Tusk <benjitusk1@gmail.com>
2022-01-02 23:57:38 +0200

    created a SearchModel

--- Yeshivat Torat Shraga/Code/Models/SearchModel.swift
+++ Yeshivat Torat Shraga/Code/Models/SearchModel.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+class SearchModel: ObservableObject {
+    func search() {
+        FirebaseConnection.searchFirestore(query: <#T##String#>, searchOptions: <#T##NSDictionary?#>, completion: <#T##((videos: [Video], audios: [Audio], rabbis: [Rabbi])?, Error?) -> Void##((videos: [Video], audios: [Audio], rabbis: [Rabbi])?, Error?) -> Void##(_ results: (videos: [Video], audios: [Audio], rabbis: [Rabbi])?, _ error: Error?) -> Void#>)
+    }
+}
