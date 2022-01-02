277de4923792294d1413cf0ebd59f19d7b34d8ca
Benji Tusk <benjitusk1@gmail.com>
2022-01-02 23:58:12 +0200

    Added components to SearchView

--- Yeshivat Torat Shraga/Code/Views/SearchView.swift
+++ Yeshivat Torat Shraga/Code/Views/SearchView.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct SearchView: View {
+    @ObservedObject var model = SearchModel()
     @State var searchText = ""
-    
+    @State var showAlert = false
     var body: some View {
         VStack {
             ZStack {
@@ -18,11 +19,14 @@ struct SearchView: View {
                     .opacity(0.1)
                 HStack {
                     Image(systemName: "magnifyingglass")
-                    TextField("", text: $searchText)
-                        .placeholder(when: searchText.isEmpty) {
-                            Text("Search...").foregroundColor(Color("ShragaGold"))
-                        }
-                    //                                .foregroundColor(Color("ShragaGold"))
+                    TextField("", text: $searchText, onCommit: {
+                        showAlert = true
+                    })
+                    .placeholder(when: searchText.isEmpty) {
+                        Text("Search...").foregroundColor(Color("ShragaGold"))
+                    }
+                    
+//                                .foregroundColor(Color("ShragaGold"))
                 }
                 .foregroundColor(Color("ShragaGold"))
                 .padding(.leading, 13)
@@ -30,13 +34,41 @@ struct SearchView: View {
             }
             .frame(height: 40)
             .cornerRadius(13)
-            .padding(.horizontal)
+            .padding()
+            Spacer()
         }
+        .alert(isPresented: $showAlert, content: {
+            Alert(title: Text("Search Submitted"), message: Text(searchText), dismissButton: Alert.Button.default(Text("OK")))
+        })
     }
 }
 
 struct SearchView_Previews: PreviewProvider {
+    
+    struct binding: View {
+        @State var presentingSearchView = true
+        var body: some View {
+            VStack {
+                HStack {Spacer()}
+                Spacer()
+                Button(action: { presentingSearchView = true }) {
+                    Text("Show Sheet")
+                        .foregroundColor(Color("ShragaGold"))
+                }
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(7)
+                .shadow(radius: 4)
+                Spacer()
+            }
+            //            .background(Color.gray.ignoresSafeArea())
+            .sheet(isPresented: $presentingSearchView) {
+                SearchView()
+                //                    .background(Color.yellow)
+            }
+        }
+    }
     static var previews: some View {
-        SearchView()
+        binding()
     }
 }
