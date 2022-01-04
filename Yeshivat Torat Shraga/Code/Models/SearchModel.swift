51a1daa49ab455e26db9436795febe8467e75966
Benji Tusk <benjitusk1@gmail.com>
2022-01-04 17:35:27 +0200

    Set SerachModel to search for and load content

--- Yeshivat Torat Shraga/Code/Models/SearchModel.swift
+++ Yeshivat Torat Shraga/Code/Models/SearchModel.swift
@@ -5,10 +5,67 @@
 //  Created by Benji Tusk on 28/12/2021.
 //
 
-import Foundation
+import SwiftUI
 
-class SearchModel: ObservableObject {
-    func search() {
-        FirebaseConnection.searchFirestore(query: <#T##String#>, searchOptions: <#T##NSDictionary?#>, completion: <#T##((videos: [Video], audios: [Audio], rabbis: [Rabbi])?, Error?) -> Void##((videos: [Video], audios: [Audio], rabbis: [Rabbi])?, Error?) -> Void##(_ results: (videos: [Video], audios: [Audio], rabbis: [Rabbi])?, _ error: Error?) -> Void#>)
+class SearchModel: ObservableObject, ErrorShower {
+    @Published var showError: Bool = false
+    @Published var rebbeim: [Rabbi] = []
+    @Published var sortables: [SortableYTSContent] = []
+    @Published var content: Content?
+    var errorToShow: Error?
+    var retry: (() -> Void)?
+
+    func search(_ query: String) {
+        print("Searching Firebase...")
+        let searchOptions: [String: Any] = [
+            "content":
+                [
+                    "limit": 10,
+                    "includeThumbnailURLs": false,
+                    "includeDetailedAuthorInfo": true,
+                    "startFromDocumentID": nil
+                    
+                ],
+            "rebbeim":
+                [
+                    "limit": 5,
+                    "includePictureURLs": false,
+                    "startFromDocumentID": nil
+                    
+                ]
+        ]
+        FirebaseConnection.searchFirestore(query: query, searchOptions: searchOptions) { results, error in
+            
+            guard let rebbeim = results?.contentAndRabbis.rebbeim else {
+                self.showError(error: error ?? YTSError.unknownError, retry: {})
+                return
+            }
+
+            
+            guard let contents = results?.contentAndRabbis.content else {
+                self.showError(error: error ?? YTSError.unknownError, retry: {})
+                return
+            }
+            
+            withAnimation {
+                
+                self.content = contents
+                for audio in self.content!.audios {
+                    self.sortables.append(audio.sortable)
+                }
+                for video in self.content!.videos {
+                    self.sortables.append(video.sortable)
+                }
+                
+                self.sortables = self.sortables.sorted(by: { lhs, rhs in
+                    return lhs.date! > rhs.date!
+                })
+
+                
+                for rabbi in rebbeim {
+                    self.rebbeim.append(rabbi)
+                }
+            }
+        }
     }
 }
