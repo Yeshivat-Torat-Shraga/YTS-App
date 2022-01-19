a25c443ab77e979500f4ea2afd4bb1f81d22f482
Eliyahu Masinter <eliyahumasinter@gmail.com>
2022-01-19 22:25:46 +0200

    Authentication system

--- Yeshivat Torat Shraga/Code/Webapp/main.py
+++ Yeshivat Torat Shraga/Code/Webapp/main.py
@@ -1,1 +1,17 @@
-import Flask
+from flask import Flask
+from flask_basicauth import BasicAuth
+import settings
+app = Flask(__name__)
+basic_auth = BasicAuth(app)
+
+app.config['BASIC_AUTH_USERNAME'] = settings.username
+app.config['BASIC_AUTH_PASSWORD'] = settings.password
+app.config['BASIC_AUTH_FORCE'] = True
+
+@app.route("/")
+def home():
+    return "Home Page"
+    
+if __name__ == "__main__":
+    app.run(debug=True)
+    
