62fa098b6f4a261e1e19dde800483c112b9eb18d
Benji Tusk <benjitusk1@gmail.com>
2022-01-22 19:43:02 +0200

    playing around with linting options

--- Yeshivat Torat Shraga/Code/functions/.eslintrc.js
+++ Yeshivat Torat Shraga/Code/functions/.eslintrc.js
@@ -1,14 +1,29 @@
 module.exports = {
   root: true,
-  env: {
-    es6: true,
-    node: true,
+  // env: {
+  //   es6: true,
+  //   node: true,
+  // },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.json", "tsconfig.dev.json"],
+    // sourceType: "module",
   },
+  plugins: [
+    "@typescript-eslint"
+  ],
+
+  ignorePatterns: [
+    "lib/**/*", // Ignore built files.
+  ],
   extends: [
-    "eslint:recommended",
+    // "eslint:recommended",
     "google",
   ],
   rules: {
-    quotes: ["error", "double"],
+    "quotes": ["error", "double"],
+    "arrow-body-style": ["error", "always"],
+    "max-len": 0,
+    "indent": 0,
   },
 };
