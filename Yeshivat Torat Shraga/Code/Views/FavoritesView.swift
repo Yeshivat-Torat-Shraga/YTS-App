f94f27cfd824ddffce729b1ff937739349c4a312
Benji Tusk <benjitusk1@gmail.com>
2022-01-28 01:33:53 +0200

    Created basic layout for favorited audios

--- Yeshivat Torat Shraga/Code/Views/FavoritesView.swift
+++ Yeshivat Torat Shraga/Code/Views/FavoritesView.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct FavoritesView: View {
+    @ObservedObject var model = FavoritesModel()
     var body: some View {
         NavigationView {
-            Text("Favorites View")
-                .navigationTitle("Favorites")
+            ScrollView {
+                VStack {
+                    HStack {
+                        Text("Audio")
+                            .font(.title3)
+                            .bold()
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                    if let audios = model.audios {
+                        ScrollView(showsIndicators: false) {
+                            HStack {
+                                ForEach(audios, id: \.self) { audio in
+                                    AudioCardView(audio: audio)
+                                        .padding(.vertical)
+                                }
+                            }
+                            .padding(.horizontal)
+                        }
+                    } else {
+                        VStack {
+                            Text("Yikes, it seems like you don't have any saved Audio Shiurim right now.")
+                        }
+                        .padding()
+                    }
+                }
+            }
         }
+        .navigationTitle("Favorites")
     }
 }
 
