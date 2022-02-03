a80f23ec238aa5b641c4bd537ad16362953247e8
David Reese <david@reesedevelopment.com>
2022-02-03 08:31:56 +0200

    changed interfaces to classes with bailable initalizers

--- Yeshivat Torat Shraga/Code/functions/src/types.ts
+++ Yeshivat Torat Shraga/Code/functions/src/types.ts
@@ -1,6 +1,8 @@
+import { isString } from "./helpers";
+
 export interface LoadData {
   metadata: {
-    lastLoadedDocID: string;
+    lastLoadedDocID: string | null;
     includesLastElement: boolean;
     error?: string;
   };
@@ -42,28 +44,74 @@ export interface ContentDocument {
   author: Author;
 }
 
-export interface NewsFirebaseDocument {
+export class NewsFirebaseDocument {
   author: string;
   body: string;
   date: Date;
   imageURLs: string[];
   title: string;
+
+  constructor(data: FirebaseFirestore.DocumentData) {
+    if (
+      isString(data.author) &&
+      isString(data.body) &&
+      data.date instanceof Date &&
+      Array.isArray(data.imageURLs) &&
+      isString(data.title)
+    ) {
+      this.author = data.author;
+      this.body = data.body;
+      this.date = data.date;
+      this.imageURLs = data.imageURLs;
+      this.title = data.title;
+    } else {
+      throw new Error("Invalid data");
+    }
+  }
 }
 
-export interface SlideshowImageFirebaseDocument {
+export class SlideshowImageFirebaseDocument {
   /** The name of the file inside of the cloud storage folder */
   image_name: string;
   title: string;
   uploaded: Date;
+
+  constructor(data: FirebaseFirestore.DocumentData) {
+    if (
+      isString(data.image_name) &&
+      isString(data.title) &&
+      data.uploaded instanceof Date
+    ) {
+      this.image_name = data.image_name;
+      this.title = data.title;
+      this.uploaded = data.uploaded;
+    } else {
+      throw new Error("Invalid data");
+    }
+  }
 }
 
-export interface RebbeimFirebaseDocument {
+export class RebbeimFirebaseDocument {
   name: string;
   profile_picture_filename: string;
   search_index: string;
+
+  constructor(data: FirebaseFirestore.DocumentData) {
+    if (
+      isString(data.name) &&
+      isString(data.profile_picture_filename) &&
+      isString(data.search_index)
+    ) {
+      this.name = data.name;
+      this.profile_picture_filename = data.profile_picture_filename;
+      this.search_index = data.search_index;
+    } else {
+      throw new Error("Invalid data");
+    }
+  }
 }
 
-export interface ContentFirebaseDocument {
+export class ContentFirebaseDocument {
   attributionID: string;
   author: string;
   date: Date;
@@ -74,11 +122,55 @@ export interface ContentFirebaseDocument {
   tags: string[];
   title: string;
   type: string;
+
+  constructor(data: FirebaseFirestore.DocumentData) {
+    if (
+      isString(data.attributionID) &&
+      isString(data.author) &&
+      data.date instanceof Date &&
+      isString(data.description) &&
+      typeof data.duration === "number" &&
+      Array.isArray(data.search_index) &&
+      isString(data.source_path) &&
+      Array.isArray(data.tags) &&
+      isString(data.title) &&
+      isString(data.type)
+    ) {
+      this.attributionID = data.attributionID;
+      this.author = data.author;
+      this.date = data.date;
+      this.description = data.description;
+      this.duration = data.duration;
+      this.search_index = data.search_index;
+      this.source_path = data.source_path;
+      this.tags = data.tags;
+      this.title = data.title;
+      this.type = data.type;
+    } else {
+      throw new Error("Invalid data");
+    }
+  }
 }
 
-export interface Author {
+export class Author {
   id: string;
   name: string;
   profile_picture_filename: string;
   profile_picture_url?: string;
+
+  constructor(data: FirebaseFirestore.DocumentData) {
+    if (
+      isString(data.id) &&
+      isString(data.name) &&
+      isString(data.profile_picture_filename) &&
+      (isString(data.profile_picture_url) || data.profile_picture_url === undefined)
+    ) {
+      this.id = data.id;
+      this.name = data.name;
+      this.profile_picture_filename = data.profile_picture_filename;
+      this.profile_picture_url = data.profile_picture_url;
+    } else {
+      throw new Error("Invalid data");
+    }
+  }
 }
