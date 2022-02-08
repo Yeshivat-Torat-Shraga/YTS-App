00452a554b779ff0f3d2311ac0fef7239f9c35c1
BenjiTusk <benjitusk1@gmail.com>
2022-02-08 12:57:23 +0200

    Created VideoPlayer file

---
+++ Yeshivat Torat Shraga/Code/Views/VideoPlayer.swift
@@ -0,0 +1,20 @@
+//
+//  VideoPlayer.swift
+//  Yeshivat Torat Shraga
+//
+//  Created by Benji Tusk on 08/02/2022.
+//
+
+import SwiftUI
+
+struct VideoPlayer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VideoPlayer_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoPlayer()
+    }
+}
