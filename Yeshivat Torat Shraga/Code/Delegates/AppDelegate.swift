53002d3dc8f3ce5f25064e0533538825ccc4dea2
BenjiTusk <benjitusk1@gmail.com>
2022-02-14 23:46:23 +0200

    Added boilerplate code for receiving Firebase notifications

--- Yeshivat Torat Shraga/Code/Delegates/AppDelegate.swift
+++ Yeshivat Torat Shraga/Code/Delegates/AppDelegate.swift
@@ -9,27 +9,32 @@ import UIKit
 import CoreData
 import Firebase
 import FirebaseStorage
+import FirebaseMessaging
 import AVKit
 import FirebaseAppCheck
 import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    
+    let gcmMessageIDKey = "gcm.message_id"
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let providerFactory: AppCheckProviderFactory
-                
-        #if targetEnvironment(simulator)
-                providerFactory = AppCheckDebugProviderFactory()
-        #else
-                providerFactory = YTSAppCheckProviderFactory()
-        #endif
-                
+        
+#if targetEnvironment(simulator)
+        providerFactory = AppCheckDebugProviderFactory()
+#else
+        providerFactory = YTSAppCheckProviderFactory()
+#endif
+        
         AppCheck.setAppCheckProviderFactory(providerFactory)
         
         FirebaseApp.configure()
-        #if EMULATORS
+        
+        FirebaseConfiguration.shared.setLoggerLevel(.min)
+#if EMULATORS
         print("""
         *************************************
         **      +  === = === = ===  +      **
@@ -38,7 +43,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         *************************************
         """)
         Functions.functions().useEmulator(withHost: "http://localhost", port: 5001)
-        #endif
+#endif
         
         let audioSession = AVAudioSession.sharedInstance()
         
@@ -48,38 +53,84 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             print("Audio session failed")
         }
         
-        registerForPushNotifications()
+        Messaging.messaging().delegate = self
+        
+        
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+        )
+        
+        
+        application.registerForRemoteNotifications()
+        
+        
         return true
     }
-
+    
+    func application(_ application: UIApplication,
+                     didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        // If you are receiving a notification message while your app is in the background,
+        // this callback will not be fired till the user taps on the notification launching the application.
+        // TODO: Handle data of notification
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        
+        // Print full message.
+        print(userInfo)
+    }
+    
+    func application(_ application: UIApplication,
+                     didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Unable to register for remote notifications: \(error.localizedDescription)")
+    }
+    
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let stringToken = deviceToken.reduce("", {$0 + String(format: "%02X", $1)}).uppercased()
+        print("APNs token retrieved: \(stringToken)")
+        
+        // With swizzling disabled you must set the APNs token here.
+        // Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         let sendingAppID = options[.sourceApplication]
         print("source application = \(sendingAppID ?? "Unknown")")
         
         // Process the URL.
-            guard let components = NSURLComponents(url: url, resolvingAgainstBaseURL: true), let albumPath = components.path, let params = components.queryItems else {
-                    print("Invalid URL or album path missing")
-                    return false
-            }
-
+        guard let components = NSURLComponents(url: url, resolvingAgainstBaseURL: true), let albumPath = components.path, let params = components.queryItems else {
+            print("Invalid URL or album path missing")
+            return false
+        }
+        
         print(url)
         return false
-//            if let photoIndex = params.first(where: { $0.name == "index" })?.value {
-//                print("albumPath = \(albumPath)")
-//                print("photoIndex = \(photoIndex)")
-//                return true
-//            } else {
-//                print("Photo index missing")
-//                return false
-//            }
+        //            if let photoIndex = params.first(where: { $0.name == "index" })?.value {
+        //                print("albumPath = \(albumPath)")
+        //                print("photoIndex = \(photoIndex)")
+        //                return true
+        //            } else {
+        //                print("Photo index missing")
+        //                return false
+        //            }
     }
     
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
@@ -88,33 +139,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
     
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        let tokenParts = deviceToken.map { data in
-            String(format: "%02.2hhx", data)
-        }
-        let token = tokenParts.joined()
-        print("Device token: \(token)")
-    }
-    
-    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        print("Failed to register: \(error)")
-    }
-
     // MARK: - Core Data stack
-
+    
     lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
          creates and returns a container, having loaded the store for the
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
-        */
+         */
         let container = NSPersistentContainer(name: "Yeshivat_Torat_Shraga")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
+                
                 /*
                  Typical reasons for an error here include:
                  * The parent directory does not exist, cannot be created, or disallows writing.
@@ -128,7 +167,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         })
         return container
     }()
-
+    
 #if EMULATORS
     func applicationWillTerminate(_ application: UIApplication) {
         print("""
@@ -141,7 +180,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 #endif
     // MARK: - Core Data Saving support
-
+    
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
@@ -155,28 +194,69 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-    
-    
-    func registerForPushNotifications() {
-        UNUserNotificationCenter.current()
-            .requestAuthorization(options: [.alert, .sound, .badge]) { [weak self] granted, _ in
-                print("Permission Granted: \(granted)")
-                guard granted else { return }
-                self?.getNotificationSettings()
-            }
-    }
-    
-    func getNotificationSettings() {
-        UNUserNotificationCenter.current()
-            .getNotificationSettings { settings in
-                print("Notification settings: \(settings)")
-                guard settings.authorizationStatus == .authorized else { return }
-                DispatchQueue.main.async {
-                    UIApplication.shared.registerForRemoteNotifications()
-                }
-            }
-    }
-    
-    
 }
 
+extension AppDelegate {
+    // Receive displayed notifications for iOS 10 devices.
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions)
+                                -> Void) {
+        let userInfo = notification.request.content.userInfo
+        
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+        // [START_EXCLUDE]
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        // [END_EXCLUDE]
+        // Print full message.
+        print(userInfo)
+        
+        // Change this to your preferred presentation option
+        completionHandler([[.banner, .list, .sound]])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        
+        // [START_EXCLUDE]
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        // [END_EXCLUDE]
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+        // Print full message.
+        print(userInfo)
+        
+        completionHandler()
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    // [START refresh_token]
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        if let fcmToken = fcmToken {
+            print("Firebase registration token: \(String(describing: fcmToken))")
+        } else {
+            print("Firebase registration token: NIL")
+        }
+        
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
+        // TODO: If necessary send token to application server.
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+    
+    // [END refresh_token]
+}
