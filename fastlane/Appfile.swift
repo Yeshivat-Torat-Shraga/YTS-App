7e4efd707277ae8531aa6c785a7e8f9ddbb5cfe0
BenjiTusk <benjitusk1@gmail.com>
2022-03-02 13:33:38 +0200

    Added FastLane

---
+++ fastlane/Appfile.swift
@@ -0,0 +1,9 @@
+var appIdentifier: String { return "com.reesedevelopment.YTS" } // The bundle identifier of your app
+var appleID: String { return "dir2100@me.com" } // Your Apple email address
+
+var itcTeam: String? { return "121811495" } // App Store Connect Team ID
+var teamID: String { return "YL279DNVQD" } // Apple Developer Portal Team ID
+
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile
