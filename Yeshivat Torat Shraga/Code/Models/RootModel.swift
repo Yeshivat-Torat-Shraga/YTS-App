1f63c35ef61b4977261aea4686708d92ed03f316
David Reese <david@reesedevelopment.com>
2022-03-19 19:54:25 +0200

    added some access modifiers

--- Yeshivat Torat Shraga/Code/Models/RootModel.swift
+++ Yeshivat Torat Shraga/Code/Models/RootModel.swift
@@ -9,16 +9,13 @@ import Foundation
 import SwiftUI
 
 class RootModel: ObservableObject, ErrorShower {
-    
     @AppStorage("firstLaunch")
     private var isFirstLaunch = true
     @Published var showOnboarding = false
         
-    var retry: (() -> Void)?
-    
+    internal var retry: (() -> Void)?
     @Published var showError: Bool = false
-    
-    var errorToShow: Error?
+    internal var errorToShow: Error?
         
     static var audioPlayer: AudioPlayer = AudioPlayer()
     static var audioPlayerBinding: Binding<AudioPlayer> = Binding {
@@ -29,16 +26,17 @@ class RootModel: ObservableObject, ErrorShower {
     
     @Published var showLoadingScreen = true
     @Published var homeView: HomeView?
+    @Published var favoritesView = FavoritesView()
+    @Published var newsView = NewsView()
+    @Published var settingsView = SettingsView()
+    
     @Published var alert: Alert?
-    var favoritesView = FavoritesView()
-    var newsView = NewsView()
-    var settingsView = SettingsView()
     
     
     init() {
         if isFirstLaunch {
-            isFirstLaunch = false
             showOnboarding = true
+            isFirstLaunch = false
         }
         let appearance = UITabBar.appearance()
         appearance.standardAppearance.backgroundEffect = UIBlurEffect(style: .systemChromeMaterial)
@@ -47,10 +45,13 @@ class RootModel: ObservableObject, ErrorShower {
             scrollEdgeAppearance.backgroundEffect = UIBlurEffect(style: .systemChromeMaterial)
             appearance.scrollEdgeAppearance = scrollEdgeAppearance
         }
-        homeView = HomeView(hideLoadingScreenClosure: {self.showLoadingScreen = false},
-                            showErrorOnRoot: { error, retry in
+        
+        self.homeView = HomeView(hideLoadingScreen: {
+            self.showLoadingScreen = false
+        }, showErrorOnRoot: { error, retry in
             self.showError(error: error, retry: retry!)
         })
+        
         RootModel.audioPlayer.refreshFavorites = {
             self.favoritesView.model.load()
         }
