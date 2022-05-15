1af6769c8495a9122c9a9e3de1795136a6b07d29
BenjiTusk <benjitusk1@gmail.com>
2022-05-15 18:35:01 +0300

    Formatting

--- Yeshivat Torat Shraga/Code/Models/Player.swift
+++ Yeshivat Torat Shraga/Code/Models/Player.swift
@@ -14,26 +14,26 @@ let timeScale = CMTimeScale(1000)
 let time = CMTime(seconds: 0.5, preferredTimescale: timeScale)
 
 final class Player: NSObject, ObservableObject {
-
+    
     /// Display time that will be bound to the scrub slider.
     @Published var displayTime: TimeInterval = 0
-
+    
     /// The observed time, which may not be needed by the UI.
     @Published var observedTime: TimeInterval = 0
-
+    
     @Published var itemDuration: TimeInterval = 0
     fileprivate var itemDurationKVOPublisher: AnyCancellable!
-
+    
     /// Publish timeControlStatus
     @Published var timeControlStatus: AVPlayer.TimeControlStatus = .paused
     fileprivate var timeControlStatusKVOPublisher: AnyCancellable!
-
+    
     /// The AVPlayer
     @Published var avPlayer: AVPlayer?
-
+    
     /// Time observer.
     fileprivate var periodicTimeObserver: Any?
-
+    
     var scrubState: PlayerScrubState = .reset {
         didSet {
             switch scrubState {
@@ -50,16 +50,16 @@ final class Player: NSObject, ObservableObject {
     override init() {
         super.init()
     }
-
+    
     init(avPlayer: AVPlayer) {
         self.avPlayer = avPlayer
         super.init()
-
+        
         self.addPeriodicTimeObserver()
         self.addTimeControlStatusObserver()
         self.addItemDurationPublisher()
     }
-
+    
     deinit {
         removePeriodicTimeObserver()
         timeControlStatusKVOPublisher?.cancel()
@@ -76,11 +76,11 @@ final class Player: NSObject, ObservableObject {
         self.addTimeControlStatusObserver()
         self.addItemDurationPublisher()
     }
-
+    
     func play() {
         self.avPlayer?.play()
     }
-
+    
     func pause() {
         self.avPlayer?.pause()
     }
@@ -100,20 +100,20 @@ final class Player: NSObject, ObservableObject {
     func setRate(_ rate: Float) {
         self.avPlayer?.rate = rate
     }
-
+    
     fileprivate func addPeriodicTimeObserver() {
         self.periodicTimeObserver = avPlayer?.addPeriodicTimeObserver(forInterval: time, queue: .main) { [weak self] (time) in
             guard let self = self else { return }
-
+            
             // Always update observed time.
             withAnimation {
-            self.observedTime = time.seconds
+                self.observedTime = time.seconds
             }
-
+            
             switch self.scrubState {
             case .reset:
                 withAnimation {
-                self.displayTime = time.seconds
+                    self.displayTime = time.seconds
                 }
             case .scrubStarted:
                 // When scrubbing, the displayTime is bound to the Slider view, so
@@ -121,13 +121,13 @@ final class Player: NSObject, ObservableObject {
                 break
             case .scrubEnded(let seekTime):
                 withAnimation {
-                self.scrubState = .reset
-                self.displayTime = seekTime
+                    self.scrubState = .reset
+                    self.displayTime = seekTime
                 }
             }
         }
     }
-
+    
     fileprivate func removePeriodicTimeObserver() {
         guard let periodicTimeObserver = self.periodicTimeObserver else {
             return
@@ -135,7 +135,7 @@ final class Player: NSObject, ObservableObject {
         avPlayer?.removeTimeObserver(periodicTimeObserver)
         self.periodicTimeObserver = nil
     }
-
+    
     fileprivate func addTimeControlStatusObserver() {
         timeControlStatusKVOPublisher = avPlayer?
             .publisher(for: \.timeControlStatus)
@@ -145,24 +145,22 @@ final class Player: NSObject, ObservableObject {
                 withAnimation {
                     self.timeControlStatus = newStatus
                 }
-                }
-        )
+            })
     }
-
+    
     fileprivate func addItemDurationPublisher() {
         itemDurationKVOPublisher = avPlayer?
             .publisher(for: \.currentItem?.duration)
             .receive(on: DispatchQueue.main)
             .sink(receiveValue: { [weak self] (newStatus) in
                 guard let newStatus = newStatus,
-                    let self = self else { return }
+                      let self = self else { return }
                 withAnimation {
-                self.itemDuration = newStatus.seconds
+                    self.itemDuration = newStatus.seconds
                 }
-                }
-        )
+            })
     }
-
+    
     enum PlayerScrubState {
         case reset
         case scrubStarted
