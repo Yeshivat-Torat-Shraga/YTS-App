bd0f49a4a555b684a2f68cea278c6fde5687cdf4
BenjiTusk <benjitusk1@gmail.com>
2022-05-15 18:37:13 +0300

    Add placeholder text for About page, see #104

--- Yeshivat Torat Shraga/Code/Views/AboutView.swift
+++ Yeshivat Torat Shraga/Code/Views/AboutView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            Text("""
+        This app was written by Torat Shraga alumni Benji Tusk and David Reese. Benji Tusk is now a student in Machon Lev, Jerusalem Collage of Technology, studying Computer Science, and is set to graduate in 2025. David Reese is studying __FieldOfStudy__ in Yeshiva University, to graduate in 20XX. David specializes in __SpecialSkills__, having worked on __PreviousProjects__ in the past. Benji has experience with a, b, c, and hopes to get a job doing __SomethingCool__.
+        
+        You can reach the developers here:
+        • Benji Tusk: __MethodOfContact__
+        • David Reese: __MethodOfContact__
+        """)
+            .padding()
+            .foregroundColor(.shragaBlue)
+            Spacer()
+        }
+        .navigationTitle("About")
     }
 }
 
