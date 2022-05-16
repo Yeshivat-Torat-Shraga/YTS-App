88563835c040d16d3f240bd19d4e83829a500e11
BenjiTusk <benjitusk1@gmail.com>
2022-05-17 00:50:07 +0300

    Updated About View

--- Yeshivat Torat Shraga/Code/Views/AboutView.swift
+++ Yeshivat Torat Shraga/Code/Views/AboutView.swift
@@ -8,164 +8,145 @@
 import SwiftUI
 import MessageUI
 
+struct Developer: Hashable {
+    let name: String
+    let githubURL: String
+    let emailAddress: String
+    let uuid = UUID()
+}
+
 struct AboutView: View {
-    @EnvironmentObject var audioPlayerModel: AudioPlayerModel
-    
+    let developers: [Developer]
+    init() {
+        self.developers = [
+            Developer(name: "David Reese",
+                      githubURL: "https://github.com/davidreese",
+                      emailAddress: "david@reesedevelopment.com"),
+            Developer(name: "Benji Tusk",
+                      githubURL: "https://github.com/benjitusk",
+                      emailAddress: "benjitusk1@gmail.com")
+        ]
+    }
     var body: some View {
         ScrollView {
             Group {
-            VStack {
-                Text("""
-                 This app was written by Torat Shraga alumni Benji Tusk and David Reese. Benji Tusk is now a student in Machon Lev, Jerusalem College of Technology, studying Computer Science, and is set to graduate in 2025. David Reese going to be learning in Yeshiva University and studying computer science, set to graduate in 2026.
+                VStack {
+                    Text("""
+                 This app was written by Torat Shraga 2022 alumni Benji Tusk and David Reese. Benji Tusk is now a student in Machon Lev, Jerusalem College of Technology, studying Computer Science, and is set to graduate in 2025. David Reese going to be learning in Yeshiva University and studying computer science, set to graduate in 2026.
                  """)
-                .padding()
-                .font(.body)
-                .foregroundColor(.black)
-                
-                Divider()
-                
-                Group {
-                HStack {
-//                    Label("Benji", systemImage: "person.crop.circle")
-                    Text("Contact Benji:")
-                        .font(Font.subheadline)
-                    Button {
-                        sendEmail(to: "benjitusk1@gmail.com")
-                    } label: {
-                        Image(systemName: "envelope")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 18, height: 18)
+                    .padding()
+                    .font(.body)
+                    .foregroundColor(.primary)
+                    
+                    VStack {
+                        ForEach(developers, id: \.uuid) { developer in
+                            HStack {
+                                Spacer()
+                                Text("Contact \(developer.name):")
+                                    .font(.subheadline)
+                                Spacer()
+                                Button(action: {
+                                    sendEmail(to: developer.emailAddress)
+                                }) {
+                                    Image(systemName: "envelope")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 18, height: 18)
+                                        .foregroundColor(.primary)
+                                        .colorInvert()
+                                }
+                                .buttonStyle(iOS14BorderedButtonStyle(color: .primary))
+                                Button(action: {
+                                    let link = URL(string: developer.githubURL)!
+                                    UIApplication.shared.open(link)
+                                }) {
+                                    Image("github")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 18, height: 18)
+                                }
+                                .buttonStyle(iOS14BorderedButtonStyle(color: .primary))
+                                Spacer()
+                            }
+                            .padding()
+                        }
+                        .background(Color(UIColor.systemGray4))
+                        .cornerRadius(UI.cornerRadius)
+                        .shadow(radius: UI.shadowRadius)
                     }
-                    .buttonStyle(iOS14BorderedButtonStyle(color: .white))
-                    Button {
-                        let link = URL(string: "https://github.com/benjitusk")!
-                        UIApplication.shared.open(link)
-                    } label: {
-                        Image("github")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 18, height: 18)
-                    }
-                    .buttonStyle(iOS14BorderedButtonStyle(color: .white))
+                    
                 }
                 .padding()
-                .frame(minWidth: 236)
-                }
-                .foregroundColor(.black)
-                .background(Color(UIColor.systemGray4))
-                .cornerRadius(UI.cornerRadius)
-                .shadow(radius: UI.shadowRadius)
-                
-                
-                Group {
-                HStack {
-                    Text("Contact David:")
-                        .font(Font.subheadline)
-                    Button {
-                        sendEmail(to: "david@reesedevelopment.com")
-                    } label: {
-                        Image(systemName: "envelope")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 18, height: 18)
-                    }
-                    .buttonStyle(iOS14BorderedButtonStyle(color: .white))
-                    Button {
-                        let link = URL(string: "https://github.com/davidreese")!
-                        UIApplication.shared.open(link)
-                    } label: {
-                        Image("github")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 18, height: 18)
-                    }
-                    .buttonStyle(iOS14BorderedButtonStyle(color: .white))
-                }
-                .padding()
-                .frame(minWidth: 236)
-                }
-                .foregroundColor(.black)
-                .background(Color(UIColor.systemGray4))
-                .cornerRadius(UI.cornerRadius)
-                .shadow(radius: UI.shadowRadius)
-                
-            }
-            .padding()
-            .background(Color.shragaBlue)
-//            .background(Color(UIColor.systemGray4))
+                .background(Color.shragaBlue)
             }
             .cornerRadius(UI.cornerRadius)
             .shadow(radius: UI.shadowRadius)
             .padding([.horizontal, .bottom])
             
             Group {
-            VStack {
-                Button(action: {
-                    let link = URL(string: "https://toratshraga.com")!
-                    UIApplication.shared.open(link)
-                }) {
-                    HStack {
-                        Spacer()
-                        Image(systemName: "safari")
-                        .resizable()
-                        .frame(width: 30, height: 30)
-                    Text("Visit the YTS Website")
-                            .font(.callout)
-                            .foregroundColor(Color(UIColor.gray))
-                        Spacer()
+                VStack {
+                    Button(action: {
+                        let link = URL(string: "https://toratshraga.com")!
+                        UIApplication.shared.open(link)
+                    }) {
+                        HStack {
+                            Spacer()
+                            Image(systemName: "safari")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                            Text("Visit the YTS Website")
+                                .font(.callout)
+                                .foregroundColor(Color(UIColor.gray))
+                            Spacer()
+                        }
                     }
-                }
-                .buttonStyle(iOS14BorderedButtonStyle(color: .white))
-                
-                Button(action: {
-                    let link = URL(string: "https://www.facebook.com/toratshraga/")!
-                    UIApplication.shared.open(link)
-                }) {
-                    HStack {
-                        Spacer()
-                    Image("facebook")
-                        .resizable()
-                        .frame(width: 30, height: 30)
-                    Text("See YTS on Facebook")
-                            .font(.callout)
-                            .foregroundColor(Color(UIColor.gray))
-                        Spacer()
-                    }
-                }
-                .buttonStyle(iOS14BorderedButtonStyle(color: .white))
-                
-                Button(action: {
-                    let link = URL(string: "https://github.com/Yeshivat-Torat-Shraga/YTS-App")!
-                    UIApplication.shared.open(link)
-                }) {
-                    HStack {
-                        Spacer()
-                    Image("github")
-                        .resizable()
-                        .frame(width: 30, height: 30)
-                    Text("YTS Source Code on Github")
-                            .font(.callout)
-                            .foregroundColor(Color(UIColor.gray))
-                        Spacer()
-                    }
-                }
-                .buttonStyle(iOS14BorderedButtonStyle(color: .white))
+                    .buttonStyle(iOS14BorderedButtonStyle(color: .white))
                     
-            }
-            .padding()
-            .background(Color.shragaBlue)
-//            .background(Color(UIColor.systemGray4))
+                    Button(action: {
+                        let link = URL(string: "https://www.facebook.com/toratshraga/")!
+                        UIApplication.shared.open(link)
+                    }) {
+                        HStack {
+                            Spacer()
+                            Image("facebook")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                            Text("See YTS on Facebook")
+                                .font(.callout)
+                                .foregroundColor(Color(UIColor.gray))
+                            Spacer()
+                        }
+                    }
+                    .buttonStyle(iOS14BorderedButtonStyle(color: .white))
+                    
+                    Button(action: {
+                        let link = URL(string: "https://github.com/Yeshivat-Torat-Shraga/YTS-App")!
+                        UIApplication.shared.open(link)
+                    }) {
+                        HStack {
+                            Spacer()
+                            Image("github")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                            Text("YTS Source Code on Github")
+                                .font(.callout)
+                                .foregroundColor(Color(UIColor.gray))
+                            Spacer()
+                        }
+                    }
+                    .buttonStyle(iOS14BorderedButtonStyle(color: .white))
+                    
+                }
+                .padding()
+                .background(Color.shragaBlue)
+                //            .background(Color(UIColor.systemGray4))
             }
             .cornerRadius(UI.cornerRadius)
             .shadow(radius: UI.shadowRadius)
             .padding()
-        
+            
             Spacer()
             
-            if audioPlayerModel.audio != nil {
-                Spacer().frame(height: UI.playerBarHeight)
-            }
         }
         .navigationTitle("About")
         .navigationBarItems(trailing: LogoView(size: .small))
@@ -173,11 +154,11 @@ struct AboutView: View {
     
     func sendEmail(to address: String) {
         if let url = URL(string: "mailto:\(address)") {
-          if #available(iOS 10.0, *) {
-            UIApplication.shared.open(url)
-          } else {
-            UIApplication.shared.openURL(url)
-          }
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
         }
 //        if MFMailComposeViewController.canSendMail() {
 //                let mail = MFMailComposeViewController()
@@ -194,5 +175,6 @@ struct AboutView: View {
 struct AboutView_Previews: PreviewProvider {
     static var previews: some View {
         AboutView()
+            .preferredColorScheme(.dark)
     }
 }
