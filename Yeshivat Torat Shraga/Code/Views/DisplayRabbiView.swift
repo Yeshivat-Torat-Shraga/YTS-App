43ccb4cb99c30e08b681df56babaa039f03e7e6d
David Reese <david@reesedevelopment.com>
2022-05-17 08:03:14 -0400

    design change on displayrabbiview

--- Yeshivat Torat Shraga/Code/Views/DisplayRabbiView.swift
+++ Yeshivat Torat Shraga/Code/Views/DisplayRabbiView.swift
@@ -61,7 +61,8 @@ struct DisplayRabbiView: View {
                                 }
                             }
                         } else {
-                            Group {
+                            HStack {
+                                Spacer()
                             VStack {
                                 Text("Sorry, no shiurim here yet.")
                                     .bold()
@@ -73,6 +74,7 @@ struct DisplayRabbiView: View {
                             }
                             .multilineTextAlignment(.center)
                             .padding()
+                                Spacer()
                             }
                             .background(Color(UIColor.systemGray4))
                             .cornerRadius(5)
