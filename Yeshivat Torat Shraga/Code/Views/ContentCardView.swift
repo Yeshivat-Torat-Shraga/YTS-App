70da63343ad7c68f21f2313c804e3a72f44d7128
BenjiTusk <benjitusk1@gmail.com>
2022-05-24 16:01:23 +0300

    updated content card ui

--- Yeshivat Torat Shraga/Code/Views/ContentCardView.swift
+++ Yeshivat Torat Shraga/Code/Views/ContentCardView.swift
@@ -70,8 +70,6 @@ struct ContentCardView<Content: YTSContent>: View {
                             HStack {
                                 Text(content.title)
                                     .font(.headline)
-                                    .bold()
-                                    .minimumScaleFactor(0.8)
                                     .lineLimit(2)
                                     .multilineTextAlignment(.leading)
                                     .fixedSize(horizontal: false, vertical: true)
