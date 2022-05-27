d801dec69a6de10104d4c5427f07698049eaa7f7
David Reese <david@reesedevelopment.com>
2022-05-27 11:38:24 -0400

    test run enviornemnt

--- Yeshivat Torat Shraga/Code/run/index.js
+++ Yeshivat Torat Shraga/Code/run/index.js
@@ -1,1 +1,12 @@
-console.log("Test")
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'World';
+  res.send(`Hello ${name}!`);
+});
+
+const port = parseInt(process.env.PORT) || 8080;
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
+});
