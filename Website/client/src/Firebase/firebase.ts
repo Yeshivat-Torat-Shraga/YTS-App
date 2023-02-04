c509cbdb4e87f92a3cdc9ce7691f751817fba726
BenjiTusk <benjitusk1@gmail.com>
2023-02-04 19:17:47 +0200

    integrated firebase storage

--- Website/client/src/Firebase/firebase.ts
+++ Website/client/src/Firebase/firebase.ts
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase/app';
 // import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getStorage } from '@firebase/storage';
 import firebaseConfig from './config.json';
 
 // Initialize Firebase
@@ -9,4 +10,5 @@ export const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
 export const firestore = getFirestore(app);
+export const storage = getStorage(app);
 export const auth = getAuth(app);
---
+++ Website/client/src/utils.ts
@@ -0,0 +1,17 @@
+import { getDownloadURL, ref } from '@firebase/storage';
+import { storage } from './Firebase/firebase';
+import { Rabbi, RawRabbi } from './types/rabbi';
+
+export async function processRawRebbeim(rawRebbeim: RawRabbi[]): Promise<Rabbi[]> {
+	return Promise.all(
+		rawRebbeim.map(async (rawRabbi) => {
+			let rabbi: Rabbi = {
+				...rawRabbi,
+				profilePictureURL: await getDownloadURL(
+					ref(storage, `profile-pictures/${rawRabbi.profile_picture_filename}`)
+				),
+			};
+			return rabbi;
+		})
+	);
+}
